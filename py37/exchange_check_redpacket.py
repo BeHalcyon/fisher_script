b241c30431c77f1d88894df3ab50e421356fc511
xiangyanghe <9506665+xiangyanghe@user.noreply.gitee.com>
2022-06-30 17:49:44 +0800

    .

---
+++ py37/exchange_check_redpacket.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+# -*- coding: utf-8 -*
+'''
+项目名称:exchange_check_coupons.py
+Author: yangyang
+功能：
+Date: 2022-5-22
+cron: 0 23 14,20 * * *
+new Env("红包通知");
+'''
+from exchange_lib import *
+
+if __name__ == '__main__':
+
+    ExchangeManagement(cookie_type="mine")
+
+    cookies = os.environ["JD_COOKIE"].split('&') if "JD_COOKIE" in os.environ else []
+
+    summary = "红包速览"
+    content = ""
+    for cookie in cookies:
+        content += findRedPacket(cookie)
+    print(content)
+    sendNotification(summary=summary, content=content)
+
+
+
